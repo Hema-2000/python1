7973238d68ac9fe27637172a88cbd5144e59b921
ch-hemalatha <hemalatha.chandaka@thundersoft.com>
2023-11-09 20:00:27 +0530

    conditional control statements

---
+++ sel_cs.py
@@ -0,0 +1,19 @@
+x = 5
+y = 6
+if x > y:
+    print("x is greater than y")
+
+if x < y:
+    print("x is less than y")
+else:
+    print("x is greater than y")
+
+if (x == y):
+    print("x is equal to y")
+elif (x < y):
+    print("x is less than y")
+else:
+    print("x is greater than y")
+
+
+

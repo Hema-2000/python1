90f12ddc63779240a0fdf5bf5cbda417a9b78555
ch-hemalatha <hemalatha.chandaka@thundersoft.com>
2023-11-09 13:15:27 +0530

    basic python codelines

---
+++ Basics.py
@@ -0,0 +1,13 @@
+a=int(input("enter the 1st number")) #taking input from user
+b=int(input("enter the second number"))
+print(a/b)  #gives float value
+print(a//b) #gives int value
+print(a**b) #a power of b
+print("hema's lappy") # string usage
+print('hema "laptop"')
+print('hema'+'laptop')
+print(5*'hema') #printing string for 5 times
+print('c : \docs\nest') #here \n represent go to next line
+print(r'c : \docs\nest') #to present as it is without considering \n , for that give  raw string)
+print(type(a)) #it gives class datatype
+print(id(a)) #it gives address of a.

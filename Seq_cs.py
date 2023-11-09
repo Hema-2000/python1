f6c97b032533decfc0dff60c615eef5a8fc7a784
ch-hemalatha <hemalatha.chandaka@thundersoft.com>
2023-11-09 19:58:05 +0530

    sequential control structure

---
+++ Seq_cs.py
@@ -0,0 +1,8 @@
+a = 20 # We will initialize some variables
+b = 10
+c = a - b
+d = a + b # We will initialize some variables
+print("The result of the subtraction is: ", c)
+print("The result of the addition is: ", d)
+
+# Execution flow will be the same as the code is written, and there is no hidden flow  in sequential cs

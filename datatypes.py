db3a6d4c7a21b2ec3095535c45a777f425fb076d
ch-hemalatha <hemalatha.chandaka@thundersoft.com>
2023-11-09 15:45:09 +0530

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+a=1
+print("Type of a: ",type(a))
+b="learn python"
+print("Type of b: ",type(b))
+c=3.0
+print("Type of c: ",type(c))
+d=3+2j
+print("Type of d: ",type(d))
+x=['pen','book','pencil'];
+print("Type of x: ",type(x))
+y=('pen','book','pencil');
+print("Type of y: ",type(y))
+z={'pen','book','pencil'};
+print("Type of z: ",type(z))
+p={'male':'father','female':'mother'};
+print("Type of p: ",type(p))
+q=True
+print("Type of q: ",type(q))
